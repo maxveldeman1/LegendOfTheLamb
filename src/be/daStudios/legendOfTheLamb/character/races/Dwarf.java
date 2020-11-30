212f6bb86b91938f83e3a6b4f7b26fa41c37dc46
Michiel Van de Vyver <michielvandevyver@gmail.com>
2020-11-30 09:52:05 +0100

    Toevoeging enum

---
+++ src/be/daStudios/legendOfTheLamb/character/races/Dwarf.java
@@ -0,0 +1,10 @@
+package be.daStudios.legendOfTheLamb.character.races;
+
+import be.daStudios.legendOfTheLamb.character.Player;
+
+public class Dwarf extends Player {
+
+
+
+
+}
---
+++ src/be/daStudios/legendOfTheLamb/character/races/Elf.java
@@ -0,0 +1,4 @@
+package be.daStudios.legendOfTheLamb.character.races;
+
+public class Elf {
+}
