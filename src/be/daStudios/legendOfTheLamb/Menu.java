6a4c60f3fa86cdd5a12086993eb5ee07680ab250
max.veldeman <max.veldeman@hotmail.com>
2020-12-02 12:46:24 +0100

    Print Map

--- src/be/daStudios/legendOfTheLamb/Menu.java
+++ src/be/daStudios/legendOfTheLamb/Menu.java
@@ -51,6 +51,7 @@ public class Menu {
                   printControls();
                   break;
               case "settings":
+
                   settingsMenu();
                   break;
           }
